e0649ef780bb1b4104a6fa35c59cd7023e5ccb8e
DanteUh <dante.uhlinder@gmail.com>
2018-01-31 13:20:14 +0100

    Add comment integration test

--- src/__tests__/Api-index.js
+++ src/__tests__/Api-index.js
@@ -1,6 +1,6 @@
 import * as api from '../api';
 import fakePosts from '../fakePosts';
-import { filterComments } from '../api';
+import fakeComments from '../fakeComments';
 
 beforeEach(() =>{
   localStorage.clear();
@@ -17,6 +17,12 @@ describe('Posts tests', () => {
     const fetchedPosts = api.fetchAllPosts();
     expect(fetchedPosts).toEqual(fakePosts.data);
   });
+  it('should not fetch all posts', () => {
+    const comments = JSON.stringify(fakePosts.data);
+    localStorage.setItem('coments', comments);
+    const fetchedComments = api.fetchAllCommments();
+    expect(fetchedComments).toEqual([]);
+  });
   it('should remove post', () => {
     const postId = '565ddy34';
     api.storePostObject(fakePosts.data);
@@ -26,17 +32,33 @@ describe('Posts tests', () => {
     const fetchedNewPosts = api.fetchAllPosts();
     expect(fetchedNewPosts.length).toBe(2);
   });
+
+  it('integration test post functions', () => {
+    const postObject1 = api.createPostObject('Scary Things', 'Things that are scary is not really scary', 'Zac');
+    const postObject2 = api.createPostObject('Bananas', 'Did you know that banans are berries?', 'Esmeralda');
+    const database = [postObject1, postObject2];
+    api.storePostObject(database);
+    const fetchedPosts = api.fetchAllPosts();
+    expect(fetchedPosts).toEqual(database);
+    api.removePost(postObject1.id);
+    const fetchedUpdatedPosts = api.fetchAllPosts();
+    expect(fetchedUpdatedPosts).toEqual([postObject2]);
+  });
 });
 
 describe('Comments tests', () => {
-
   it('should store comment object', () => {
     const commentObject = {comment: 'Hello Mr. Bister!', postId: 1, author: 'Hej'};
     api.storeCommentObject(commentObject);
     const storageItem = JSON.parse(localStorage.getItem('comments'));
     expect(storageItem).toEqual(commentObject);
   });
-
+  it('should not fetch all comments', () => {
+    const comments = JSON.stringify(fakeComments.data);
+    localStorage.setItem('bananer', comments);
+    const fetchedComments = api.fetchAllCommments();
+    expect(fetchedComments).toEqual([]);
+  });
   it('should filter comments', () => {
     const comments = [
       {comment: 'Hejpådig!', postId: 1, author: 'Steffe'},
